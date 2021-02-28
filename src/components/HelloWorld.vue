d1215284fe9dec47535e3a23a159a891ca7a3d60
Sofija <sofija11@gmail.com>
2021-02-28 22:56:33 +0100

    dificulty radio buttons, and start button with random number working together

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,75 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+      <div class="wrap">
+        <div class="wrap_flexxx" v-for="(diff, index) in difficulty" :key="index">
+          <input type="radio" :value="choosenTiming" name="diffValues" @input="chooseDificulty(diff.timing)" class="radioBtn"/>{{ diff.name}}
+        </div>
+        <button type="button" class="btnStart" @click="startGame()">Start Game</button>
+        <!-- Random numbers-->
+        <span v-if="choosenNumber">{{ choosenNumber }}</span>
+      </div>
+      <div class="wrap_flex" v-for="(letter, letterIndex) in lettersArray" :key="letterIndex" :class="[]">
+          <div>
+            <span class="upperText">{{ letter }}</span> (<span > {{ letterIndex+1 }}</span>)
+          </div>
+      </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String,
+  name: 'Game',
+  data: function(){
+      return {
+      difficulty: [
+        {
+          name: 'Easy',
+          timing: 5000,
+        },
+        {
+          name: 'Medium',
+          timing: 3500,
+        },
+        {
+          name: 'Hard',
+          timing: 2000,
+        }
+      ],
+      lettersArray: null,
+      numbersArray: null,
+      choosenTiming: null,
+      choosenNumber: null,
+    }
   },
+  created: function () {
+    this.lettersArray = [...'abcdefghijklmnopqrstuvwxyz'];
+    this.numbersArray = [...Array(26).keys()];
+    console.log(this.numbersArray);
+  },
+   methods: {
+    chooseDificulty(timing) {
+      this.choosenTiming = timing;
+    },
+    startGame() {
+      const self = this;
+        setInterval(function() {
+            self.choosenNumber = self.numbersArray[Math.floor(Math.random()*self.numbersArray.length)];
+            //filter array remove duplicates
+      }, self.choosenTiming);
+    },
+    stopGame() {
+    }
+  }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.wrap_flex {
+  display: flex;
+  justify-content: space-between;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.upperText {
+  text-transform: uppercase;
 }
 </style>
