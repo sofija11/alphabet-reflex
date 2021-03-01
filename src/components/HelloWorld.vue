e3c5bdc6478c162d12bbdf6e86fadfa04409473f
Sofija <sofija11@gmail.com>
2021-03-01 07:30:48 +0100

    red and green color after input

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,21 +1,23 @@
 <template>
   <div>
       <div class="wrap">
-        <div class="wrap_flexxx" v-for="(diff, index) in difficulty" :key="index">
-          <input type="radio"
-          :value="choosenTiming"
-          name="diffValues" 
-          :class="[gameStart ? 'disabled' : '']"
-          @input="chooseDificulty(diff.timing)"
-          class="radioBtn"/>{{ diff.name}}
+        <div class="radio_flex" v-for="(diff, index) in difficulty" :key="index">
+          <span><input type="radio"
+                :value="choosenTiming"
+                name="diffValues"
+                :class="[gameStart ? 'disabled' : '']"
+                @input="chooseDificulty(diff.timing)"
+                class="radioBtn"/>{{ diff.name}}
+          </span>
         </div>
-         <input type="button" class="btnStart" @click="startGame()" :value="[ gameStart ? 'Stop' : 'Start Game']">
+         <input type="button" :disabled="choosenTiming === null" class="btnStart" @click="startGame()" :value="[ gameStart ? 'Stop' : 'Start Game']">
         <!-- Random numbers-->
         <span v-if="choosenNumber">{{ choosenNumber }}</span>
+        <input type="text" :class="[gameStart ? '' : 'disabled']" placeholder="Input letter" v-model="typedLetter" @input="checkScore($event)"/>
       </div>
       <div class="wrap_flex" v-for="(letter, letterIndex) in lettersArray" :key="letterIndex" :class="[]">
           <div>
-            <span class="upperText">{{ letter }}</span> (<span > {{ letterIndex+1 }}</span>)
+              <span :class="[greenLetter[letterIndex] === true ? 'green' : '', redLetter[letterIndex] === true ? 'red' : '']" class="upperText">{{ letter }} ( {{letterIndex+1 }})</span>
           </div>
       </div>
   </div>
@@ -46,6 +48,9 @@ export default {
       choosenNumber: null,
       gameStart: false,
       intervalID: null,
+      typedLetter: null,
+      redLetter: [],
+      greenLetter: [],
     }
   },
   created: function () {
@@ -72,7 +77,27 @@ export default {
         this.gameStart = false;
       }
     },
-   
+    checkScore(event) {
+      this.typedLetter = event.target.value;
+      const foundLetter = this.lettersArray.find(element => element === this.typedLetter.toLowerCase());
+      const indexLett = this.lettersArray.findIndex(element => element === foundLetter);
+      if(foundLetter)  {
+        if(indexLett === this.choosenNumber) {
+          console.log('Score!!!');
+          this.$set(this.greenLetter, indexLett, !this.greenLetter[indexLett]);
+          setTimeout(function() {
+            this.typedLetter = "";
+          }.bind(this), 500);
+        }
+        else {
+          console.log('Fail!!!');
+          this.$set(this.redLetter, indexLett, !this.redLetter[indexLett]);
+          setTimeout(function() {
+            this.typedLetter = "";
+          }.bind(this), 500);
+        }
+      }
+    }
   }
 };
 </script>
@@ -81,12 +106,68 @@ export default {
 <style scoped>
 .wrap_flex {
   display: flex;
+  justify-content: unset;
+  width: 50%;
+  margin: 0 auto;
+  flex-wrap: wrap;
+ 
+}
+.letter {
+  min-width: 65px;
+  padding: 20px;
+
+}
+.radio_flex {
+  display: contents;
   justify-content: space-between;
+}
+.btnStart {
+  width: auto;
+  padding: 5px 10px;
+  height: 30px;
+  border: 1px solid violet !important;
+  color: violet;
+  font-size: 12px;
+  background: white;
+  display: block;
+  margin: 0 auto;
+  position: relative;
+  top: 20px;
+}
+.btnStart:disabled:hover{
+  cursor: default;
+}
+input[type=text] {
+  padding: 5px 10px;
+  height: 15px;
+  color: gray;
+  border: 1px solid #dedede;
+  margin-top: 25px;
+}
+input[type=text]:focus, input[type=text]:hover, .btnStart:focus {
+  box-shadow: none;
+  border: 1px solid lightskyblue;
+  outline: none;
+}
+.btnStart:hover {
+  cursor: pointer;
 }
 .upperText {
   text-transform: uppercase;
+  font-weight: bold;
 }
-.radioBtn.disabled {
+.radioBtn.disabled,  input[type=text].disabled {
   pointer-events: none;
+
+}
+.green {
+  color: green;
+}
+.red {
+  color: red;
+}
+.randomNumber {
+  padding: 15px;
+  font-size: 35px;
 }
 </style>
